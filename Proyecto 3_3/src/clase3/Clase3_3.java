4b159e3f67c2225d6c92d3a9ade0e550c25067cc
alvaroMG-212 <alvaromonge2005@gmail.com>
2024-11-04 23:26:07 +0100

    hola

---
+++ Proyecto 3_3/src/clase3/Clase3_3.java
@@ -0,0 +1,12 @@
+package clase3;
+
+public class Clase3_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Practica 3_3 Alvaro Monge ");
+	}
+
+}
